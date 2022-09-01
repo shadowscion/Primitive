f582b4ef268ef492fc0318a070c89cb686e09041
shadowscion <shadowscion@users.noreply.github.com>
2022-09-01 08:01:36 -0500

    Update shapes.lua

--- lua/primitive/entities/shapes.lua
+++ lua/primitive/entities/shapes.lua
@@ -11,18 +11,6 @@ do
         end
 
         defaults = {
-            -- greeble_plate = {
-            --     PrimDT = 4,
-            --     PrimMAXSEG = 16,
-            --     PrimMESHSMOOTH = 0,
-            --     PrimNUMSEG = 16,
-            --     PrimSIDES = 0,
-            --     PrimSIZE = Vector( 48, 48, 48 ),
-            --     PrimSUBDIV = 8,
-            --     PrimTX = 0,
-            --     PrimTY = 0,
-            --     PrimTYPE = "greeble_plate",
-            -- },
             generic = {
                 PrimDT = 4,
                 PrimMAXSEG = 16,
@@ -200,6 +188,7 @@ do
             { category = "shapes", entity = "primitive_shape", title = "wedge", command = "wedge 1 48" },
             { category = "shapes", entity = "primitive_shape", title = "wedge_corner", command = "wedge_corner 1 48" },
             --{ category = "shapes", entity = "primitive_shape", title = "greeble_plate", command = "greeble_plate 1 48" },
+            --{ category = "shapes_extra", entity = "primitive_shape", title = "ridge_plate", command = "ridge_plate 1 48" },
         }
 
         local callbacks = {
@@ -305,7 +294,7 @@ do
     local spawnlist
     if CLIENT then
         spawnlist = {
-            { category = "extra", entity = "primitive_airfoil", title = "airfoil", command = "" },
+            { category = "shapes_extra", entity = "primitive_airfoil", title = "airfoil", command = "" },
         }
 
         local callbacks = {
@@ -324,6 +313,125 @@ do
     Primitive.funcs.registerClass( "airfoil", class, spawnlist )
 end
 
+
+--[=====[
+
+--[[
+do
+    local class = {}
+
+    local typen = { "ridge_plate" }
+    local typek, defaults = {}, {}
+
+    do
+        for k, v in pairs( typen ) do
+            typek[v] = v
+        end
+
+        defaults = {
+            ridge_plate = {
+                PrimSIZE = Vector( 48, 48, 2 ),
+                PrimTYPE = "ridge_plate",
+            },
+
+            generic = {
+                PrimSIZE = Vector( 48, 48, 48 ),
+            },
+
+        }
+    end
+
+
+    function class:PrimitiveOnSetup( initial, args )
+        if initial and SERVER then
+            duplicator.StoreEntityModifier( self, "mass", { Mass = 100 } )
+        end
+
+        local type, physics, uv = unpack( args )
+
+        if defaults[type] then
+            self:SetPrimTYPE( type )
+
+            for k, v in pairs( defaults.generic ) do
+                local set = defaults[type][k]
+                if set == nil then set = v end
+
+                if set ~= nil then
+                    self["Set" .. k]( self, set )
+                end
+            end
+
+            if physics ~= nil then self:SetPrimMESHPHYS( tobool( physics ) ) end
+            if tonumber( uv ) then self:SetPrimMESHUV( tonumber( uv ) ) end
+        end
+    end
+
+
+    function class:PrimitiveGetConstruct()
+        return self:PrimitiveGetConstructSimple( self.primitive.keys.PrimTYPE )
+    end
+
+
+    function class:PrimitiveSetupDataTables()
+        self:PrimitiveVar( "PrimTYPE", "String", { category = "modify", title = "type", panel = "combo", values = typek, icons = "primitive/icons/%s.png" }, true )
+        self:PrimitiveVar( "PrimSIZE", "Vector", { category = "modify", title = "size", panel = "vector", min = Vector( 1, 1, 1 ), max = Vector( 1000, 1000, 1000 ) }, true )
+
+        -- self:PrimitiveVar( "PrimDT", "Float", { category = "modify", title = "thickness", panel = "float", min = 1, max = 1000 }, true )
+        -- self:PrimitiveVar( "PrimTX", "Float", { category = "modify", title = "taper x", panel = "float", min = -1, max = 1 }, true )
+        -- self:PrimitiveVar( "PrimTY", "Float", { category = "modify", title = "taper y", panel = "float", min = -1, max = 1 }, true )
+
+        -- self:PrimitiveVar( "PrimSUBDIV", "Int", { category = "modify", title = "subdivide", panel = "int", min = 1, max = 32 }, true )
+        -- self:PrimitiveVar( "PrimMAXSEG", "Int", { category = "modify", title = "max segments", panel = "int", min = 1, max = 32 }, true )
+        -- self:PrimitiveVar( "PrimNUMSEG", "Int", { category = "modify", title = "num segments", panel = "int", min = 1, max = 32 }, true )
+        -- self:PrimitiveVar( "PrimSIDES", "Int", { category = "modify", title = "sides", panel = "bitfield", lbl = { "front", "rear", "left", "right", "top", "bottom" } }, true )
+    end
+
+
+    local spawnlist
+    if CLIENT then
+        spawnlist = {
+            { category = "shapes_extra", entity = "primitive_shape_extra", title = "ridge_plate", command = "ridge_plate 1 48" },
+        }
+
+        local callbacks = {
+            EDITOR_OPEN = function ( self, editor, name, val )
+                for k, cat in pairs( editor.categories ) do
+                    if k == "debug" or k == "mesh" or k == "model" then cat:ExpandRecurse( false ) else cat:ExpandRecurse( true ) end
+                end
+            end,
+            PrimTYPE = function( self, editor, name, val )
+                if defaults[val] == nil then
+                    return
+                end
+
+                local edit = self:GetEditingData()
+                for k, row in pairs( editor.rows ) do
+                    row:SetVisible( edit[k].global or defaults[val][k] ~= nil )
+                end
+
+                if self.primitive.keys[name] ~= val then
+                    for k, v in pairs( defaults.generic ) do
+                        local set = defaults[val][k]
+                        if set == nil then set = v end
+
+                        if set ~= nil then
+                            self:EditValue( k, tostring( set ) )
+                        end
+                    end
+                end
+
+                editor:InvalidateChildren( true )
+            end,
+        }
+
+        function class:EditorCallback( editor, name, val )
+            if callbacks[name] then callbacks[name]( self, editor, name, val ) end
+        end
+    end
+
+    Primitive.funcs.registerClass( "shape_extra", class, spawnlist )
+end
+]]
 
 
 --[[
@@ -358,3 +466,82 @@ registerType( "greeble_plate", function( param, data, threaded, physics )
     return model
 end )
 ]]
+
+--[[
+-- ridge_plate
+registerType( "ridge_plate", function( param, data, threaded, physics )
+    local model = simpleton.New()
+
+    local dx = ( isvector( param.PrimSIZE ) and param.PrimSIZE[1] or 1 )
+    local dy = ( isvector( param.PrimSIZE ) and param.PrimSIZE[2] or 1 ) * 0.5
+    local dz = ( isvector( param.PrimSIZE ) and param.PrimSIZE[3] or 1 )
+
+    local dt = math_clamp( param.PrimDT or 1, 1, 50 )
+
+    print( dt, dz )
+
+    local subdiv = math_floor( param.PrimSUBDIV or 32 )
+    if subdiv < 1 then subdiv = 1 elseif subdiv > 32 then subdiv = 32 end
+
+    for i = 0, subdiv do
+        local x = ( dx / subdiv ) * i
+        local z = i % 2 == 0 and dt or 0
+
+        local a = model:PushXYZ( x - dx * 0.5, -dy, z )
+        local b = model:PushXYZ( x - dx * 0.5, dy, z )
+        local c = model:PushXYZ( x - dx * 0.5, -dy, z + dz )
+        local d = model:PushXYZ( x - dx * 0.5, dy, z + dz )
+
+        if CLIENT then
+            if i < subdiv then
+                -- bottom
+                model:PushTriangle( a + 1, a + 5, a + 4 )
+                model:PushTriangle( a + 1, a + 4, a )
+
+                -- top
+                model:PushTriangle( c, c + 4, c + 5 )
+                model:PushTriangle( c, c + 5, c + 1 )
+
+                -- sides
+                model:PushTriangle( a, a + 4, a + 6 )
+                model:PushTriangle( a, a + 6, a + 2 )
+
+                model:PushTriangle( b + 6, b + 4, b )
+                model:PushTriangle( b + 6, b, b + 2 )
+
+                if i == 0 then
+                    model:PushTriangle( a, c, d )
+                    model:PushTriangle( a, d, b )
+                end
+            else
+                model:PushFace( a, b, d, c )
+            end
+        end
+    end
+
+    if physics then
+        local verts = model.verts
+
+        model.convexes = {}
+
+        for i = 1, #verts - 4, 4 do
+            model.convexes[#model.convexes + 1] = {
+                verts[i],
+                verts[i + 1],
+                verts[i + 2],
+                verts[i + 3],
+                verts[i + 4],
+                verts[i + 5],
+                verts[i + 6],
+                verts[i + 7],
+            }
+        end
+    end
+
+    util_Transform( model.verts, param.PrimMESHROT, param.PrimMESHPOS, threaded )
+
+    return model
+end )
+]]
+
+--]=====]
