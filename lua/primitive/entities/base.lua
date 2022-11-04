0bbd5501c15fcdfffce85d1fc7145afb0c49b770
shadowscion <shadowscion@users.noreply.github.com>
2022-11-04 15:40:06 -0500

    Update base.lua

--- lua/primitive/entities/base.lua
+++ lua/primitive/entities/base.lua
@@ -598,6 +598,23 @@ do
         end
     end
 
+    hook.Add( "AdvDupe_FinishPasting", "Primitive_FinishPasting", function( data )
+        if not istable( data ) or not istable( data[1] ) or not istable( data[1].CreatedEntities ) then return end
+
+        for _, ent in pairs( data[1].CreatedEntities ) do
+
+            -- dupe bug only affects parented primitives
+            -- if parented, delay the initialization
+
+            if ent.IsPrimitive and istable( ent.primitive ) then
+                ent:PrimitiveReconstruct()
+
+                ent.PRIMITIVE_FROM_DUPE = nil
+                ent.primitive.init = nil
+            end
+        end
+    end)
+
 end
 
 
